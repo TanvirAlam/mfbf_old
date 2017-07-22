7bc2cb0fd05728534cba16f721b2ca5799ac4f3f
Tanvir Alam <ta@casalogic.dk>
2017-07-22 11:37:33 +0200

    Fix the possibility for transaction table rollback

--- database/migrations/2017_07_21_213820_create_transactions_table.php
+++ database/migrations/2017_07_21_213820_create_transactions_table.php
@@ -32,6 +32,6 @@ class CreateTransactionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('transactions');
     }
 }
