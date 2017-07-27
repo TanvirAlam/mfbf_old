ddfd91c3be1969db603ed11b0d59a5fe59db5b27
Sydgren <henrik@henriknordquist.dk>
2017-07-28 01:31:46 +0200

    Clean up assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,10 +12,6 @@
     <p>Welcome to this beautiful admin panel.</p>
 @stop
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
 @section('js')
     <script> console.log('Hi!'); </script>
 @stop
