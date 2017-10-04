8054c943c97f5a31ca98d85de2cdb0106f775387
Tanvir Alam <ta@casalogic.dk>
2017-10-04 20:43:52 +0200

    Fix the mail template

--- resources/views/notifications/user/verification-required.blade.php
+++ resources/views/notifications/user/verification-required.blade.php
@@ -1,20 +1,20 @@
 @component('mail::message')
-    @component('mail::panel')
-        # Welcome mfbf.dk
+@component('mail::panel')
+# Welcome mfbf.dk
 
-        Thanks for registering with mfbf.dk. **We really appreciate** it.
-        Let us knwo how we can improve mfbf.dk
+Thanks for registering with mfbf.dk. **We really appreciate** it.
+Let us knwo how we can improve mfbf.dk
 
-        @component('mail::button', ['url' => sprintf('/verifyemail/%s', $user->email_token)])
-            Click to verify {{ $user->email }}
-        @endcomponent
+@component('mail::button', ['url' => sprintf('/verifyemail/%s', $user->email_token)])
+Click to verify {{ $user->email }}
+@endcomponent
 
-    @endcomponent
+@endcomponent
 
-    @component('mail::button', ['url' => 'http://mfbf.dk/feedback'])
-        Give feedback
-    @endcomponent
+@component('mail::button', ['url' => 'http://mfbf.dk/feedback'])
+Give feedback
+@endcomponent
 
-    Thanks, <br>
-    {{ config('app.name') }}
+Thanks, <br>
+{{ config('app.name') }}
 @endcomponent
