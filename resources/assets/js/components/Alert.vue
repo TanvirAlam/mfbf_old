c271879a46228bcaf2a3af59cd39f55d37aa4586
Tanvir Alam <ta@casalogic.dk>
2017-10-09 21:24:22 +0200

    Create separate component for alert message

---
+++ resources/assets/js/components/Alert.vue
@@ -0,0 +1,34 @@
+<template>
+    <article class="message is-success" v-show="show">
+        <slot></slot>
+    </article>
+</template>
+
+<script>
+    export default {
+      props: {
+        timeout: { default: 3000 }
+      },
+
+      data () {
+        return { show: true }
+      },
+
+      mounted () {
+        setTimeout(
+          () => this.show = false,
+          this.timeout
+        )
+      },
+
+      methods: {
+        hide () {
+
+        }
+      }
+    }
+</script>
+
+<style>
+
+</style>
