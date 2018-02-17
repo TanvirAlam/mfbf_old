7f37ae845b2d23bc366e76162003c97f5d07445a
Tanvir Alam <ta@casalogic.dk>
2018-02-17 11:37:49 +0100

    Makeover the dashboard

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,10 +6,13 @@ import VeeValidate from 'vee-validate'
 import VueSweetAlert from 'vue-sweetalert'
 import './utils/interceptors'
 import Buefy from 'buefy'
+import Datepicker from 'vuejs-datepicker'
+
 
 Vue.use(VeeValidate)
 Vue.use(VueSweetAlert)
 Vue.use(Buefy)
+Vue.use(require('vue-moment'));
 
 Vue.component('icon', require('./components/Icon'))
 Vue.component('alert', require('./components/Alert'))
--- resources/assets/js/components/Income/IncomeDashboard.vue
+++ resources/assets/js/components/Income/IncomeDashboard.vue
@@ -1,15 +1,53 @@
 <template>
     <article class="tile is-child box">
-        <div class="level-left">
-            <img src="/img/income_new.png" alt="User Image">
-            <label class="label">Income</label>
+        <div class="columns">
+            <div class="column is-narrow">
+                <div class="level-left">
+                    <img class="income-color" src="/img/income.png" alt="User Image">
+                    <label class="label level-left">Income</label>
+                </div>
+            </div>
+            <div class="column is-narrow">
+                <div class="level-right" style="width: 125px;">
+                    <div class="control has-icons-left has-icons-right">
+                        <datepicker
+                                input-class="input"
+                                :minimumView="'month'"
+                                :placeholder="'Month'"
+                                :format="customFormatter"
+                                @selected="showIncomeAmount">
+                        </datepicker>
+                        <span class="icon is-left">
+                            <img class="income-color" src="/img/calendar.png" alt="User Image">
+                        </span>
+                    </div>
+                    <!--<a class="button is-small">
+                        <span class="icon is-small">
+                           <icon class="fa fa-arrow-right"></icon>
+                        </span>
+                    </a>-->
+                </div>
+            </div>
         </div>
         <div class="income-amount income income-color">
-            <span class="icon">
-                <icon class="fa fa-plus"></icon>
+
+            <!--{{ formatAmount(incomeAmount) }}-->
+
+            <div class="iCountUp">
+                <span class="icon">
+                <img class="income-color" src="/img/money.png" alt="User Image">
             </span>
-            {{ formatAmount(incomeAmount) }}
+                <ICountUp
+                        :startVal="startVal"
+                        :endVal="endVal"
+                        :decimals="decimals"
+                        :duration="duration"
+                        :options="options"
+                        @ready="onReady"
+                />
+            </div>
         </div>
+        <hr/>
         <div class="is-pulled-right">
             <a class="button is-success is-small" @click="isViewIncomeModalActive = true">
                 <span class="icon is-small">
@@ -35,23 +73,48 @@
 <script>
     import IncomeAddFormModal from './IncomeAddFormModal'
     import IncomeListModal from './IncomeListModal'
+    import Datepicker from 'vuejs-datepicker'
+    import moment from 'moment'
+    import ICountUp from 'vue-countup-v2'
 
     export default {
       components: {
         IncomeAddFormModal,
-        IncomeListModal
+        IncomeListModal,
+        Datepicker,
+        ICountUp
       },
       data() {
         return {
           isInsertIncomeModalActive: false,
           isViewIncomeModalActive: false,
-          incomeAmount: 30000000,
+          startVal: 0,
+          endVal: 150000000,
+          decimals: 0,
+          duration: 2.5,
+          options: {
+            useEasing: true,
+            useGrouping: true,
+            separator: '.',
+            decimal: ',',
+            suffix: '/-DKK'
+          }
         }
       },
       methods: {
         formatAmount(value) {
           let val = (value/1).toFixed(2).replace('.', ',')
           return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+        },
+        customFormatter(date) {
+          return moment(date).format('MMM YY')
+        },
+        onReady(instance, CountUp) {
+          //const that = this;
+          instance.update(this.endVal + 100);
+        },
+        showIncomeAmount() {
+          this.endVal = 800000
         }
       }
     }
@@ -67,7 +130,17 @@
     .income-color {
         -webkit-filter: opacity(.5) drop-shadow(0 0 0 green);
     }
-    .income-amount::after {
-        content:"DKK";
+    .columns:not(:last-child) {
+         margin-bottom: 0;
+    }
+    .input {
+        border: 1px solid transparent;
+        border-radius: 5px;
+        box-shadow: none;
+        font-size: 1rem;
+        height: 2.25em;
+        font-weight: bold;
+        text-align: left;
+        width: 100%;
     }
 </style>
